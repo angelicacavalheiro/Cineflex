3b1c561decd9a31f3845c822e78e1b65d88cdd7c
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-09-01 13:53:49 -0300

    rotas adicionadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./css/style.css";
+import "./css/reset.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SelecionaFilme from "./SelecionaFilme";
+import SelecionaHorario from "./SelecionaHorario";
+import SelecionaAssento from "./SelecionaAssento";
+import ExibePedido from "./ExibePedido";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+
+        <Route >
+          <SelecionaFilme />
+        </ Route> 
+
+        <Route >
+          <SelecionaHorario />
+        </ Route> 
+
+        <Route >
+          <SelecionaAssento />
+        </ Route> 
+
+        <Route >
+          <ExibePedido />
+        </ Route> 
+
+      </ Switch>       
+    </ BrowserRouter>  
   );
 }
 
---
+++ src/SelecionaFilme.js
@@ -0,0 +1,10 @@
+import logotipo from "./assets/logotipo.jpeg";
+
+export default function SelecionaFilme(){
+    return(
+        <div>
+            texto
+            <img src={logotipo} />
+        </div>
+    )
+}
